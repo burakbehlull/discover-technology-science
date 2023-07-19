23ab3962ae674676c4650ab274ba75a1f7186b9c
31000s <burakyabgu@gmail.com>
2023-07-19 16:11:49 +0300

    feat: Set up site body and input contents

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     extend: {
     	fontFamily: {
     		Nunito: ['Nunito', 'sans-serif'],
-    	}
+    	},
+    	flex: {
+    		'half': '1 50%',
+        '70': '70%',
+        '30': '30%',
+    	},
+      height: {
+        '112': '28rem'
+      }
     },
   },
   plugins: [],
